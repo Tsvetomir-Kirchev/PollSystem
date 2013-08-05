4d7675456827db3b2be43efc1fec9251064650ad
Tsvetomir-Kirchev <tsvetomir.kirchev@gmail.com>
2013-08-05 15:34:39 +0300

    Added Poll controller and AllPolls action

---
+++ PollSystem/PollSystem/Controllers/PollController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PollSystem.Controllers
+{
+    public class PollController : Controller
+    {
+        //
+        // GET: /Poll/
+
+        public ActionResult AllPolls()
+        {
+            return View();
+        }
+
+    }
+}
